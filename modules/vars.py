aabbccc8f0cd279b4566b5e2c6801b863399a40c
jilanirabbani1234-coder <jilanirabbani1234@gmail.com>
2025-10-31 11:03:52 +0530

    Remove default values from environment variables
    Removed default values for API_ID, API_HASH, BOT_TOKEN, OWNER, and CREDIT.

--- modules/vars.py
+++ modules/vars.py
@@ -3,20 +3,21 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "29426008"))
-API_HASH = environ.get("API_HASH", "fedd630ba4bd77044ee4e5a00e5300e6")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8291699148:AAEAI9nBzrJ39PNe0smKnxngO89bDD3Tdxw")
+API_ID = int(environ.get("API_ID", ""))
+API_HASH = environ.get("API_HASH", ")
+BOT_TOKEN = environ.get("BOT_TOKEN", ":")
 
-OWNER = int(environ.get("OWNER", "5504044604"))
-CREDIT = environ.get("CREDIT", "𝐒𝐂𝐇𝐑𝐎𝐃𝐈𝐍𝐆𝐄𝐑")
+OWNER = int(environ.get("OWNER", ""))
+CREDIT = environ.get("CREDIT", "")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '5504044604').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '5504044604').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
+
 
 
